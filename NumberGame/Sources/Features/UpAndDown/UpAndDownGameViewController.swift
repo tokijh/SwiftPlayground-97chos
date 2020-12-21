6408c00b7e8d64f6d0f698eca1cb491b31b7ee89
윤중현 <tokijh@naver.com>
2020-12-22 01:53:35 +0900

    빈 Up&Down 게임 화면 추가

---
+++ NumberGame/Sources/Features/UpAndDown/UpAndDownGameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UpAndDownGameViewController.swift
+//  NumberGame
+//
+//  Created by 윤중현 on 2020/12/22.
+//
+
+import UIKit
+
+final class UpAndDownGameViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Up & Down"
+  }
+}
