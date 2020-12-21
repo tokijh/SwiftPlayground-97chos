7ea8bd7c68fe395d1920313ee87494ee05aca12a
윤중현 <tokijh@naver.com>
2020-12-22 01:35:56 +0900

    GameCell 을 선택했을 때의 효과 제거

--- NumberGame/Sources/Features/GameList/View/GameCell.swift
+++ NumberGame/Sources/Features/GameList/View/GameCell.swift
@@ -9,7 +9,23 @@ import UIKit
 
 final class GameCell: UITableViewCell {
 
+  // MARK: Initializing
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.configure()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+
   // MARK: Configuring
+
+  private func configure() {
+    self.selectionStyle = .none
+  }
 
   func set(title: String) {
     self.textLabel?.text = title
