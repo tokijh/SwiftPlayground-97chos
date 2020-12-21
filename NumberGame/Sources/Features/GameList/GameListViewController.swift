57290ff5766c4801f96e28cf734420d46f3ceac0
윤중현 <tokijh@naver.com>
2020-12-22 01:33:32 +0900

    게임 목록 화면에서 게임 목록을 볼 수 있음

--- NumberGame/Sources/Features/GameList/GameListViewController.swift
+++ NumberGame/Sources/Features/GameList/GameListViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 final class GameListViewController: UIViewController {
 
+  // MARK: Constants
+
+  private enum Constant {
+    static let games = ["Number Game"]
+  }
+
+  private enum ReuseIdentifier {
+    static let gameCell = "gameCell"
+  }
+
+
   // MARK: UI
 
   private let tableView = UITableView()
@@ -29,7 +40,13 @@ final class GameListViewController: UIViewController {
   // MARK: Configuring
 
   private func configureViews() {
+    self.configureTableView()
     self.layoutViews()
+  }
+
+  private func configureTableView() {
+    self.tableView.register(GameCell.self, forCellReuseIdentifier: ReuseIdentifier.gameCell)
+    self.tableView.dataSource = self
   }
 
 
@@ -41,5 +58,21 @@ final class GameListViewController: UIViewController {
     self.tableView.snp.makeConstraints {
       $0.edges.equalToSuperview()
     }
+  }
+}
+
+extension GameListViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return Constant.games.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let gameCell = tableView.dequeueReusableCell(
+      withIdentifier: ReuseIdentifier.gameCell,
+      for: indexPath
+    ) as? GameCell else { return UITableViewCell() }
+    let gameTitle = Constant.games[indexPath.item]
+    gameCell.set(title: gameTitle)
+    return gameCell
   }
 }
