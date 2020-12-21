1227b794bace0b441006a2b2aa4cb05622eede03
윤중현 <tokijh@naver.com>
2020-12-22 01:03:58 +0900

    GameListViewController 뷰 레이아웃 추가

--- NumberGame/Sources/Features/GameList/GameListViewController.swift
+++ NumberGame/Sources/Features/GameList/GameListViewController.swift
@@ -7,15 +7,41 @@
 
 import UIKit
 
+import SnapKit
+
 final class GameListViewController: UIViewController {
+
+  // MARK: UI
+
+  private let collectionView = UICollectionView()
+
 
   // MARK: Initializing
 
   init() {
     super.init(nibName: nil, bundle: nil)
+    self.configureViews()
   }
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+
+  // MARK: Configuring
+
+  private func configureViews() {
+    self.layoutViews()
+  }
+
+
+  // MARK: Layout
+
+  private func layoutViews() {
+    self.view.addSubview(collectionView)
+
+    self.collectionView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
   }
 }
