c3e24e5d26fa8107d29b787ba712ec795c7535c6
윤중현 <tokijh@naver.com>
2021-01-07 16:11:38 +0900

    화면 레이아웃

--- NumberGame/Sources/Features/UpAndDown/UpAndDownGameViewController.swift
+++ NumberGame/Sources/Features/UpAndDown/UpAndDownGameViewController.swift
@@ -8,8 +8,93 @@
 import UIKit
 
 final class UpAndDownGameViewController: UIViewController {
+
+  // MARK: UI
+
+  private lazy var descriptionLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+    label.text = "1~100까지 중에서 숨겨진 하나의 숫자를 찾는 게임입니다."
+    return label
+  }()
+  private lazy var inputNumberLabel: UILabel = { // 입력한 숫자
+    let label = UILabel()
+    label.font = UIFont.preferredFont(forTextStyle: .body)
+    label.text = "X 보다"
+    label.textAlignment = .center
+    return label
+  }()
+  private lazy var inputNumberStateLabel: UILabel = { // Up, Down 상태
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 50, weight: .heavy)
+    label.textAlignment = .center
+    label.text = "Up 👍"
+    return label
+  }()
+  private lazy var inputCountLabel: UILabel = { // N 번 입력
+    let label = UILabel()
+    label.font = UIFont.preferredFont(forTextStyle: .body)
+    label.textColor = .systemGray
+    label.textAlignment = .center
+    label.text = "N번 입력했습니다."
+    return label
+  }()
+  private lazy var button: UIButton = { // 입력하기, 다시시작을 할 수 있는 버튼
+    let button = UIButton()
+    button.setTitle("입력하기", for: .normal)
+    button.backgroundColor = .systemBlue
+    return button
+  }()
+
+
+  // MARK: Configuring
+
+  private func configure() {
+    self.title = "Up & Down"
+  }
+
+  // MARK: View Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.title = "Up & Down"
+    self.view.backgroundColor = .systemBackground
+    self.configure()
+    self.layout()
+  }
+
+
+  // MARK: Layout
+
+  private func layout() {
+    let windowSafeAreaInsets = UIApplication.shared.windows.first(where: \.isKeyWindow)?.safeAreaInsets ?? .zero
+
+    self.view.addSubview(self.descriptionLabel)
+    self.view.addSubview(self.inputNumberLabel)
+    self.view.addSubview(self.inputNumberStateLabel)
+    self.view.addSubview(self.inputCountLabel)
+    self.view.addSubview(self.button)
+
+    self.descriptionLabel.snp.makeConstraints {
+      $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+    self.inputNumberLabel.snp.makeConstraints {
+      $0.top.equalTo(self.descriptionLabel.snp.bottom).offset(56)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+    self.inputNumberStateLabel.snp.makeConstraints {
+      $0.top.equalTo(self.inputNumberLabel.snp.bottom).offset(32)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+    self.inputCountLabel.snp.makeConstraints {
+      $0.top.equalTo(self.inputNumberStateLabel.snp.bottom).offset(40)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+    self.button.snp.makeConstraints {
+      $0.width.equalToSuperview()
+      $0.height.equalTo(56 + windowSafeAreaInsets.bottom)
+      $0.bottom.equalToSuperview()
+    }
+    self.button.contentEdgeInsets.bottom = windowSafeAreaInsets.bottom
   }
 }
