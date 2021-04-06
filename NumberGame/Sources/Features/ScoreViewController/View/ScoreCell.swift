5e614883dc64b4a5eb5d3130df8cad01625dec1c
조상호 <97chos@naver.com>
2021-04-06 23:57:23 +0900

    Score Cell에 이름 라벨 추가 및 UI 변경

--- NumberGame/Sources/Features/ScoreViewController/View/ScoreCell.swift
+++ NumberGame/Sources/Features/ScoreViewController/View/ScoreCell.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class ScoreCell: UITableViewCell {
+
+  // MARK: UI
+
+  private let playDate: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 13)
+    return label
+  }()
+  private let playCount: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 15)
+    label.textColor = .systemGray2
+    return label
+  }()
+  private let playerName: UILabel = {
+    let label = UILabel()
+    label.font = .systemFont(ofSize: 17, weight: .bold)
+    return label
+  }()
 
 
   // MARK: Initializing
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.layout()
     self.configure()
   }
 
@@ -29,8 +50,31 @@ class ScoreCell: UITableViewCell {
     self.accessoryType = .disclosureIndicator
   }
 
-  func set(title: String, subTitle: String) {
-    self.textLabel?.text = title
-    self.detailTextLabel?.text = subTitle
+  func set(date: String, count: String, name: String) {
+    self.playDate.text = date
+    self.playCount.text = count
+    self.playerName.text = name
+  }
+
+  // MARK: Layout
+
+  private func layout() {
+    self.contentView.addSubview(self.playCount)
+    self.contentView.addSubview(self.playDate)
+    self.contentView.addSubview(self.playerName)
+
+    self.playerName.snp.makeConstraints {
+      $0.bottom.equalTo(self.contentView.snp.centerY).offset(-5)
+      $0.leading.equalToSuperview().inset(20)
+    }
+    self.playDate.snp.makeConstraints {
+      $0.top.equalTo(self.contentView.snp.centerY).offset(5)
+      $0.leading.equalToSuperview().inset(20)
+    }
+    self.playCount.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.trailing.equalToSuperview().inset(10)
+    }
   }
 }
+
